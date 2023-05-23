783deab5f42c782a0955797f70b6f11a345ae409
Kitaro786 <samsundertalon@gmail.com>
2023-05-23 05:47:32 -0700

    Fixed all code

--- shellbasi/get_cmd.c
+++ shellbasi/get_cmd.c
@@ -1,56 +1,63 @@
 #include "main.h"
+
 /**
- * get_cmd - 
+ * get_cmd -
  * @input: user input
+ * @av: list of command line arguments
  * Return: pointer to args
  */
 
 char **get_cmd(char *input, char **av)
 {
-	unsigned int i = 0, j;
-	char *token, *buff = NULL, **path = NULL, **args = NULL;
+    unsigned int i = 0, j;
+    char *token, *buff = NULL, **path = NULL, **args = NULL;
 
-	token = strtok(input, " ");
-	path = malloc(1000 * sizeof(char *));
-	if (path == NULL)
-	{
-		perror(av[0]);
-		exit(0);
-	}
+    if (input == NULL) {
+        perror("Invalid input");
+        exit(EXIT_FAILURE);
+    }
 
-	while (token != NULL && i < 1000)
-	{
-		path[i] = strdup(token);
-		i++;
-		token = strtok(NULL, " ");
-	}
+    token = strtok(input, " ");
+    path = malloc(1000 * sizeof(char *));
+    if (path == NULL) {
+        perror(av[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	if (strncmp(path[0], "/bin/", strlen("/bin/")) != 0)
-	{
-		/*if input doesnt include path adds: /bin/ */
-		buff = malloc(strlen("/bin/") + strlen(path[0]) + 1);
-		if (buff == NULL)
-		{
-			perror(av[0]);
-			exit(0);
-		}
-		strcpy(buff, "/bin/");
-		strcat(buff, path[0]);
-		path[0] = buff;
-	}
-	args = malloc((i + 1) * sizeof(char *));
-	if (args == NULL)
-	{
-		perror(av[0]);
-		exit(0);
-	}
-	for (j = 0; j < i; j++)
-	{
-		args[j] = path[j];
-	}
-	args[j] = NULL;
+    while (token != NULL && i < 1000) {
+        path[i] = strdup(token);
+        if (path[i] == NULL) {
+            perror(av[0]);
+            exit(EXIT_FAILURE);
+        }
+        i++;
+        token = strtok(NULL, " ");
+    }
 
-	free(buff);
-	free(path);
-	return (args);
+    if (strncmp(path[0], "/bin/", strlen("/bin/")) != 0) {
+        /*if input doesn't include path, add: /bin/ */
+        buff = malloc(strlen("/bin/") + strlen(path[0]) + 1);
+        if (buff == NULL) {
+            perror(av[0]);
+            exit(EXIT_FAILURE);
+        }
+        strcpy(buff, "/bin/");
+        strcat(buff, path[0]);
+        path[0] = buff;
+    }
+
+    args = malloc((i + 1) * sizeof(char *));
+    if (args == NULL) {
+        perror(av[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    for (j = 0; j < i; j++) {
+        args[j] = path[j];
+    }
+    args[j] = NULL;
+
+    free(buff);
+    free(path);
+    return args;
 }
--- shellbasi/get_input.c
+++ shellbasi/get_input.c
@@ -3,53 +3,61 @@
 /**
  * _putchar - prints to stdout
  * @c: char to print
- * Return: 1 on succes
+ * Return: 1 on success
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
 /**
  * get_input - prints prompt if shell interactive & gets user input
  * @ac: no. of arguments
  * @av: list of arguments
- * Return: 1 on success
+ * Return: pointer to input on success, NULL on failure or EOF
  */
 char *get_input(int ac, char **av)
 {
-	unsigned int i;
-	char prompt[] = "Shell$ ", *input = NULL;
-	size_t l;
-	ssize_t r;
-	(void)ac;
-	(void)av;
+    unsigned int i;
+    char prompt[] = "Shell$ ";
+    char *input = NULL;
+    size_t l = 0;
+    ssize_t r;
 
-	if (isatty(STDIN_FILENO))
-	{
-		for (i = 0; prompt[i] != '\0'; i++)
-		{
-			_putchar(prompt[i]);
-		}
-	}
+    (void)ac;
+    (void)av;
 
-	r = getline(&input, &l, stdin);
-	if (r == -1 || input == NULL)
-	{
-		free(input);
-		return (NULL);
-	}
-	if (feof(stdin))
-	{
-		free(input);
-		exit(0);
-	}
-	strtok(input, "\n");
-	if (strcmp(input, "exit") == 0)
-	{
-		free(input);
-		exit(-1);
-	}
+    if (isatty(STDIN_FILENO))
+    {
+        for (i = 0; prompt[i] != '\0'; i++)
+        {
+            _putchar(prompt[i]);
+        }
+    }
 
-	return (input);
+    r = getline(&input, &l, stdin);
+    if (r == -1 || input == NULL)
+    {
+        free(input);
+        return NULL;
+    }
+
+    if (feof(stdin))
+    {
+        free(input);
+        exit(0);
+    }
+
+    if (input[strlen(input) - 1] == '\n')
+    {
+        input[strlen(input) - 1] = '\0';
+    }
+
+    if (strcmp(input, "exit") == 0)
+    {
+        free(input);
+        exit(-1);
+    }
+
+    return input;
 }
